01b40083b2c09034a7c6a00e66e78b6a195fb4ea
Swagat Mishra <swagatmishra@Swagats-MacBook-Pro.local>
2022-02-13 03:11:00 +0530

    Jenkinsfile updated

--- karma.conf.js
+++ karma.conf.js
@@ -38,8 +38,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    CHROME_BIN: '/usr/bin/google-chrome',
+    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessCI'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
